3053b08706eb39429c27cddb760e4efa9d9669a7
jacky60677 <jacky60677@gmail.com>
2023-05-31 12:01:36 +0800

    v.4(not yet)

--- app/src/main/java/com/example/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -3,32 +3,43 @@ package com.example.finalexam
 import android.graphics.Paint.Align
 import android.media.ImageReader.OnImageAvailableListener
 import android.os.Bundle
+import android.util.Log
+import android.view.View.X
+import android.view.View.Y
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.finalexam.ui.theme.FinalexamTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,8 +83,26 @@ fun Box1() {
         )
     }
 }
+
+@OptIn(ExperimentalComposeUiApi::class)
+
 @Composable
 fun Box2(){
+    val context = LocalContext.current
+    var X = remember { mutableStateOf(0f) }
+    var Y = remember { mutableStateOf(0f) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInteropFilter { event ->
+                X.value = event.getX(0)
+                Y.value = event.getY(0)
+                true
+
+            }
+    )
+
     Canvas(
         modifier = Modifier
             .size(size = 300.dp)
@@ -82,9 +111,17 @@ fun Box2(){
             color = Color.Blue,
             size = Size(width = 10.dp.toPx(), height = 10.dp.toPx()),
             topLeft = Offset(x = 540.dp.toPx(), y = 300.dp.toPx())
+
         )
+
+
+        Toast.makeText(context, "清水南社社區", Toast.LENGTH_SHORT).show()
     }
+
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
