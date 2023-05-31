2822fd22640568472076a87f1034790990f6c98f
jacky60677 <jacky60677@gmail.com>
2023-05-31 10:39:22 +0800

    v.2

--- app/src/main/java/com/example/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.finalexam
 
+import android.graphics.Paint.Align
+import android.media.ImageReader.OnImageAvailableListener
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.finalexam.ui.theme.FinalexamTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +37,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column( Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "作者 : 資管二B 陳丞彥",  textAlign = TextAlign.Center,)
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "map",
+
+            )
+    }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
